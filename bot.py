cdfa3b198aea56b48c56e4345a80c2a98ded18b6
icyspiritt <152437140+icyspiritt@users.noreply.github.com>
2024-08-14 19:46:38 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+token = ""
+bot = commands.Bot(command_prefix= "/", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"hola, soy {bot.user}")
+
+@bot.command(name="hi")
+async def hello(ctx, nombre):
+    await ctx.send(f"hola:, {bot.user} un gusto {nombre}")
+
+
+bot.run (token) 
