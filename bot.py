64dcf201662d214650e3946f134eb9a2d73fe35b
icyspiritt <152437140+icyspiritt@users.noreply.github.com>
2024-08-14 19:50:21 -0500

    last part.py

--- bot.py
+++ bot.py
@@ -13,6 +13,10 @@ async def on_ready():
 @bot.command(name="hi")
 async def hello(ctx, nombre):
     await ctx.send(f"hola:, {bot.user} un gusto {nombre}")
-
+@bot.command(name = 'psw')
+async def password(ctx,a:str,b:int):
+    nombre = str(a)
+    cr = ab.gen_pass(b)
+    await ctx.send(f'Su contraseña es: {cr}{nombre}')
 
 bot.run (token) 
